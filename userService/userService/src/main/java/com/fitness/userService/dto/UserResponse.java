63fa697d02c61ce16f85eed7053476ef5210f917
FrostScythe <maheshwariayush371@gmail.com>
2025-11-28 17:25:44 +0530

    User Service setup

---
+++ userService/userService/src/main/java/com/fitness/userService/dto/UserResponse.java
@@ -0,0 +1,14 @@
+package com.fitness.userService.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class UserResponse {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ userService/userService/src/main/java/com/fitness/userService/services/UserService.java
@@ -0,0 +1,40 @@
+package com.fitness.userService.services;
+
+import com.fitness.userService.dto.RegisterRequest;
+import com.fitness.userService.dto.UserResponse;
+import com.fitness.userService.models.User;
+import com.fitness.userService.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserResponse register(RegisterRequest request) {
+
+        if(userRepository.existsByEmail(request.getEmail())){
+            throw new IllegalArgumentException("Email already exists");
+        }
+        // Implementation for user registration
+        User user= new User();
+        user.setEmail(request.getEmail());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPassword(request.getPassword());
+
+        User savedUser = userRepository.save(user);
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+
+        return userResponse;
+    }
+}
