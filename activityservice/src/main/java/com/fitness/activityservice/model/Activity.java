7a7d35cbf8fcd20027d373ff7d05244811e240e0
FrostScythe <maheshwariayush371@gmail.com>
2025-11-30 17:51:25 +0530

    Activity Services completed

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -0,0 +1,31 @@
+package com.fitness.activityservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Document(collection = "activities")
+@Data
+@Builder // Lombok annotation to generate getters, setters, toString, etc.
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+    @Id
+    private String id;
+    private String userId;
+    private ActivityType type;
+    private Integer duration; // in minutes
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    @Field("metrics")
+    private Map<String, Object> additionalMetrics;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,48 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        LocalDateTime now = LocalDateTime.now();
+        Activity activity =Activity.builder().
+                userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .createdAt(now)
+                .updatedAt(now)
+                .build();
+        
+        Activity savedActivity = activityRepository.save(activity);
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity activity) {
+        ActivityResponse response =new ActivityResponse();
+        response.setId(activity.getId());
+        response.setUserId(activity.getUserId());
+        response.setType(activity.getType());
+        response.setDuration(activity.getDuration());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMetrics(activity.getAdditionalMetrics());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatedAt(activity.getUpdatedAt());
+        return response;
+    }
+}
