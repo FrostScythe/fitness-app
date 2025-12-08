de3995a27be8e38bc4377b87b14da78b0ef3c08d
FrostScythe <maheshwariayush371@gmail.com>
2025-12-08 16:13:07 +0530

    Bug fixed

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -0,0 +1,20 @@
+package com.fitness.aiservice.service;
+
+
+import com.fitness.aiservice.model.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}",groupId = "activity-processor-group")
+    public void processActity(Activity activity){
+        log.info("Received activity: {}", activity.getUserId());
+        // Further processing logic can be added here
+    };
+}
